51189650fb2d411039a3f7d6241471ba48cc0295
Martintcasco <martinchod91@gmail.com>
2023-05-08 18:30:14 -0300

    html-css-Home

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require ('path');
+
+const app = express();
+
+const publicPath = path.join(__dirname, "./public")
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () =>{
+    console.log('Servidor andando');
+})
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
