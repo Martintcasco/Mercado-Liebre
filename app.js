970d0ad131bf8d5e6d075ff0bbf8c052f5b26ebd
Martintcasco <martinchod91@gmail.com>
2023-05-08 19:25:20 -0300

    html-css-Login

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ app.listen(3000, () =>{
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './views/home.html'))
 })
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
