6754158b5fce643d792550cf49ee3d078cb77588
Martintcasco <martinchod91@gmail.com>
2023-05-08 19:57:54 -0300

    html-css-create-product

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const publicPath = path.join(__dirname, "./public")
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () =>{
-    console.log('Servidor andando');
-})
-
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './views/home.html'))
 })
@@ -22,3 +18,11 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'));
 });
+
+app.get('/create-product', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/create-product.html'));
+});
+
+app.listen(3000, () =>{
+    console.log('Servidor andando');
+})
